c174f805351aa1ae61d2196f1c31cc166bcbbe3d
stephanievegas <stephanierosevegasss@gmail.com>
2026-04-10 20:51:00 +0200

    15.commit?

--- app.js
+++ app.js
@@ -6,3 +6,45 @@
     nav.classList.toggle('active');
 });
 
+
+
+    const skills = [
+        "images/ae.png",
+        "images/id.png",
+        "images/ai.png",
+        "images/lr.png",
+        "images/ps.png",
+        "images/canva.png",
+        "images/figma.png"
+    ];
+
+    let index = 0;
+
+    const mainSkill = document.getElementById("mainSkill");
+    const prev = document.getElementById("prev");
+    const next = document.getElementById("next");
+
+    // næste
+    next.addEventListener("click", () => {
+        index++;
+        if(index >= skills.length){
+            index = 0;
+        }
+        mainSkill.src = skills[index];
+    });
+
+    // forrige
+    prev.addEventListener("click", () => {
+        index--;
+        if(index < 0){
+            index = skills.length - 1;
+        }
+        mainSkill.src = skills[index];
+    });
+
+    // klik på små icons
+    function changeSkill(img){
+        mainSkill.src = img.src;
+    }
+
+
